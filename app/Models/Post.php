7d5a997329305c9a60d43d9ff1dbdffc9b87dc0b
scofieldmikel <shoyemimichealiyonu@gmail.com>
2022-01-03 16:12:30 +0100

    nova validation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,5 +18,9 @@ class Post extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
     }
 }
--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -10,6 +10,7 @@ use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Boolean;
 use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class Post extends Resource
@@ -37,6 +38,27 @@ class Post extends Resource
         'id', 'title', 'body'
     ];
 
+    
+    public function title(){
+        return $this->title . ' - ' . $this->category;
+    }
+
+    public function subtitle(){
+        return 'Author: '. $this->user->name;
+    }
+
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('user_id', $request->user()->id);
+    }
+
     /**
      * Get the fields displayed by the resource.
      *
@@ -48,27 +70,35 @@ class Post extends Resource
         return [
             ID::make(__('ID'), 'id')->sortable(),
 
-            Text::make('Title'),
+            Text::make('Title')->rules('required'),
 
 
-            Trix::make('Body'),
+            Trix::make('Body')->rules('required'),
 
 
-            DateTime::make('Publish At')->hideFromIndex(),
+            DateTime::make('Publish At')->hideFromIndex()
+            ->rules('after_or_equal:today'),
             // DateTime::make('Publish Post At', 'publish_at'),  //Change convention
 
-            DateTime::make('Publish Until')->hideFromIndex(),
+            DateTime::make('Publish Until')->hideFromIndex()
+            ->rules('after_or_equal:publish_at'),
 
 
-            Boolean::make('Is Published'),
+            Boolean::make('Is Published')
+                ->canSee(function ($request){
+                    // return $reqest->user()->can('publish_post', $this);
+                    return false;
+                }),
 
 
             Select::make('category')->options([
                 'tutorials' => 'Tutorials',
                 'news' => 'News'
-            ])->hideWhenUpdating(),
+            ])->hideWhenUpdating()->rules('required'),
 
-            BelongsTo::make('User'),
+            BelongsTo::make('User')->rules('required'),
+
+            BelongsToMany::make('Tags')
 
         ];
     }
